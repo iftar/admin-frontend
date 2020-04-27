cb062b1346e815bf23aebbe056e6289b75636dc5
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2020-04-27 02:40:36 +0100

    add router

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className="Login">
+      <nav className="navbar sticky-top navbar-light bg-light">
+        <div className="container">
+          <a className="navbar-brand" href="/">Share Iftar Admin</a>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Login from './views/Login/Login';
+import Navbar from './components/navbar/Navbar';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const isLoggedIn = false;
+
+const authenticatedRoutes = (
+  <>
+  <Navbar />
+  <div className="container pt-4">
+    <Router>
+      <Switch>
+        <Route path="/">
+          <h1>Logged in</h1>
+        </Route>
+      </Switch>
+    </Router>
+  </div>
+  </>
 );
+
+const unauthenticatedRoutes = (
+  <>
+  <Navbar />
+  <div className="container pt-4">
+    <Login />
+  </div>
+  </>
+);
+
+ReactDOM.render((isLoggedIn ? authenticatedRoutes : unauthenticatedRoutes),document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
